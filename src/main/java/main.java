103dd0dd839545141f3a6ed37f7e9c5b4111097f
artemiy <karelskiy@mail.ru>
2023-07-13 16:49:15 +0300

    Task2

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(2);
+        list.add(9);
+        list.add(0);
+        list.add(-1);
+        list.print();
+        list.revers();
+        list.print();
+    }
+}
